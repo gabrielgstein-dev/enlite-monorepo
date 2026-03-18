a8ab6665799e84c8c3941e2f9efdd14219939245
Gabriel Stein <gabriel.g.stein@gmail.com>
2026-03-18 13:58:08 -0300

    Remove unnecessary ESLint disable comment from SaveStepPayload interface

--- src/infrastructure/http/WorkerApiService.ts
+++ src/infrastructure/http/WorkerApiService.ts
@@ -30,7 +30,6 @@ export interface InitWorkerPayload {
 export interface SaveStepPayload {
   workerId: string;
   step: number;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: Record<string, any>;
 }
 
